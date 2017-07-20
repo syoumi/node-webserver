0a6f651acf47e7916d676fcfab3941f118e84492
El Mahdi SYOUMI <mahdi.syoumi@gmail.com>
2017-07-20 11:36:11 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+
+app.set('view engine', 'hbs');
+app.use((req, res, next) => {
+    console.log(req.ip, + ', ' + (new Date().toString()));
+    next();
+});
+// app.use((req, res, next) => {
+//     console.log(req.ip, + ', ' + (new Date().toString()));
+//     res.render('maintenance.hbs');
+// });
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+
+app.get('/', (req, res) => {
+    // res.send('<h1>Hello express!</h1>');
+    res.render('home.hbs', {
+        page: 'Home',
+        welcome: 'Achetez ce que vous voulez'
+    });
+
+    // res.header('content-type', 'text/json');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        page: 'About'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    var err = {
+        errorMessage: 'Bad request'
+    };
+
+    res.send(err);
+});
+
+app.listen(9090, () => {
+    console.log('Server waiting for connections');
+});
